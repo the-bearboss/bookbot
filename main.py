03a639e4b4ba0abfd824080191eb072074b6a5df
BearBoss <sir@thebearboss.com>
2026-01-02 13:47:48 -0800

    added sort and report feature

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_num_characters, sort_dicts
 
 def get_book_text(filename):
     with open(filename) as f:
@@ -7,17 +7,20 @@ def get_book_text(filename):
     return book_contents
 
 def main():
-   book_filename = "books/frankenstein.txt"
-   book = get_book_text("books/frankenstein.txt")
-   char_nums = get_num_characters(book)
+    book_filename = "books/frankenstein.txt"
+    book = get_book_text("books/frankenstein.txt")
+    char_nums = get_num_characters(book)
 
-   print("============ BOOKBOT ============")
-   print(f"Analyzing book found at {book_filename}...")
-   print("----------- Word Count ----------")
-   print(f"Found {get_num_words(book)} total words")
-   print("--------- Character Count -------")
-#print sorted list here
-   print("============= END ===============")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_filename}...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(book)} total words")
+    print("--------- Character Count -------")
+    #print sorted list here
+    for ch in sort_dicts(char_nums):
+        if ch["char"].isalpha():
+            print(f"{ch['char']}: {ch['num']}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,17 +1,29 @@
+def sort_on(item):
+    return item["num"]
+
 def get_num_words(book_contents):
     word_list = book_contents.split()
 
-    return(len(word_list))
+    return len(word_list)
 
 def get_num_characters(book_contents):
     #create empty dictionary
     char_count = {}
 
     #count the letters
-    for letters in book_contents.lower():
-        if letters in char_count:
-            char_count[letters] += 1
+    for ch in book_contents.lower():
+        if ch in char_count:
+            char_count[ch] += 1
         else:
-            char_count[letters] = 1
+            char_count[ch] = 1
 
-    return (char_count)
+    return char_count
+
+def sort_dicts(incoming):
+    unsorted = []
+
+    for ch,num in incoming.items():
+        unsorted.append({"char": ch, "num": num})
+    unsorted.sort(key=sort_on, reverse=True)
+
+    return unsorted
