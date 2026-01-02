f98fb78ba4816c8461ef26176a7d6664520b1ad2
BearBoss <sir@thebearboss.com>
2026-01-02 11:48:18 -0800

    added get_num_characters function

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+from stats import get_num_words, get_num_characters
+
+def get_book_text(filename):
+    with open(filename) as f:
+        book_contents = f.read()
+    
+    return book_contents
+
+def main():
+    book = get_book_text("books/frankenstein.txt")
+    words = get_num_words(book)
+    print(f"Found {words} total words")
+    char_nums = get_num_characters(book)
+    print(char_nums)
+    
+
+main()
